d0fdc6db983920e790162a19bb019378f85f247e
arthurbrinkmann <120050691+arthurbrinkmann@users.noreply.github.com>
2026-01-27 21:04:18 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+// main.js
+
+// Closes the sidebar menu
+$("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Scrolls to the selected menu item on the page
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (
+            location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') ||
+            location.hostname === this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+// Makes interest pictures the same height
+$(window).on("load resize", function() {
+    var pictureHeight = $("#car-picture").height();
+    $(".picture").css('height', pictureHeight + "px");
+});
